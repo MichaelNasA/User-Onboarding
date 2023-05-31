35eb4d624c441fd8987b4ef0fabbb5e1835fc41d
Michael Adamson <michaelnjadamson@outlook.com>
2023-05-31 14:38:42 -0400

    rewatch the video

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState }from 'react';
+
+import axios from 'axios'
+
+
+import schema from './validation/formSchema';
+import * as yup from 'yup';
+
+import Form from './Components/Form'
+
+const initialFormValues = {
+  username: '',
+  password: '',
+  email: '',
+  tos: false
+}
+
+const initialFormErrors = {
+  username: '',
+  password: '',
+  email: '',
+  tos: ''
+}
+
 
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFromErrors] = useState(initialFormErrors);
+  const [users, setUsers] = useState([])
+
+  const handelSubmit = () =>{
+    axios.post('https://reqres.in/api/user', formValues)
+    .then(res =>{
+      .setUsers([ res.data, ...users])
+      .finally(() => setFormValues(initialFormValues))
+    })
+  }
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFromErrors({...formErrors, [name]: ''}))
+      .catch(err => setFromErrors({...formErrors, [name]: err.errors[0] }))
+
+  }
+
+  const handelChange = (name, value) =>{
+    validate(name, value);
+    setFormValues({...formValues, [name]: value});
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+      values={formValues} 
+      change={handelChange} 
+      errors={formErrors} 
+      submit={handelSubmit}
+      />
+      {users.map(user => (
+        <div key={user.id}>
+          <p>{user.createdAt}</p>
+          <p>{user.email}</p>
+        </div>
+      ))}
     </div>
   );
 }
